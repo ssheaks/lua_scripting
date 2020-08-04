5eb80e0d126ff7a0f74a3e2c5a3a6d5bb721a1e8
ssheaks <sarah.sheaks@gmail.com>
2020-08-04 16:30:12 -0700

    tables as matrices

--- notes_practice/tables.lua
+++ notes_practice/tables.lua
@@ -120,13 +120,10 @@ local found = false
 for i,v in ipairs(prince_codes) do
     print(i, v)
     print(v.Page)
-    for k,val in pairs(v)do
-        print(k,val, v)
-        if (v.Page == page and v.Line == line and v.Word == word) then
-            print("Your code is "..v.Code)
-            found = true
-            return v.Code
-        end
+    if (v.Page == page and v.Line == line and v.Word == word) then
+        print("Your code is "..v.Code)
+        found = true
+        return v.Code
     end
 end
 
@@ -134,3 +131,16 @@ end
 if not found then
     print("Sorry this combination does not exist")
 end
+
+-----------------------------
+-- Tables as Matrices
+-----------------------------
+local M = {
+    {3.4, 2.0, 2.0},
+    {-3.5, 3.3, 0.5},
+    {0.1, 0.1, 3.3}
+}
+
+print(M[1][1].." "..M[1][2].." "..M[1][3])
+print(M[2][1].." "..M[2][2].." "..M[2][3])
+print(M[3][1].." "..M[3][2].." "..M[3][3])
